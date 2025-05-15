70651383d448300ae36dad87a855158bc3fc9193
Stanislav.Kankin <stanislav.kankin@gmail.com>
2025-05-15 17:14:14 +0300

    добавил ссылки

--- utils/promts.py
+++ utils/promts.py
@@ -21,3 +21,139 @@ def get_system_prompt(section: str, knowledge: str) -> str:
     База знаний раздела:
     {knowledge}
     """
+
+
+TEST = """
+Перелючение в режим "Сотрудник" - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Lichnyi-kabinet-/Perekliuchenie-v-rezhim-«Sotrudnik»-/
+Загрузить документ - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Zagruzitь-dokument/
+Настроить маршрут согалсования документа - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Nastroitь-marshrut-soglasovaniia-dokumenta-/
+Добавить в документ более одного сотрудника - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Dobavitь-v-dokument-bolee-odnogo-sotrudnika-/
+Отправить документ на подпись - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Otpravitь-dokument-na-podpisь/
+Удалить или аннулировать документ - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Udalitь-ili-annulirovatь-dokument-/
+Скачать архив документооборота - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Skachatь-arkhiv-dokumentooborota-/
+Скачать архивы КЭДО - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Massovoe-skachivanie-arkhivov-dokumentooborota/
+Добавить комментарий к документу - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Dobavitь-kommentarii-k-dokumentu-/
+Добавить заметки к документу - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Dobavitь-zametki-k-dokumentu/
+Использовать фильтры в реестре документов - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Ispolьzovatь-filьtry-v-reestre-dokumentov/
+Загрузить ЛНА - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/LNA-/Zagruzitь-LNA/
+Утвердить ЛНА у руководителя - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/LNA-/Utverditь-LNA-u-rukovoditelia/
+Отправить один ЛНА выбранным сотрудникам - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/LNA-/Otpravitь-odin-LNA-vybrannym-sotrudnikam/
+Отправить несколько ЛНА выбранным сотрудникам - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/LNA-/Otpravitь-neskolьko-LNA-vybrannym-sotrudnikam/
+Управлять параметрами ЛНА: автоотправка, префиксы, отображение после подписания - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/LNA-/Upravliatь-parametrami-LNA%3A-avtootpravka%2C-prefiksy%2C-otobrazhenie-posle-podpisaniia/
+Настроить фильтры реестра - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Zaiavleniia/Nastroitь-filьtry-reestra/
+Обработать или отклонить заявление - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Zaiavleniia/Obrabotatь-ili-otklonitь-zaiavlenie-/
+Добавить связь между документом и заявлением - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Zaiavleniia/Dobavitь-sviazь-mezhdu-dokumentom-i-zaiavleniem-/
+Добавить сотрудника на портал - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Dobavitь-sotrudnika-na-portal-/
+Пригласить сотрудников на портал - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Priglasitь-sotrudnikov-na-portal-/
+Проверка СМЭВ перед выпуском УНЭП - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Proverka-SMEV-pered-vypuskom-UNEP/
+Инициировать выпуск УНЭП - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Vypustitь-UNEP-sotrudniku-/
+Редактировать данные сотрудника - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Redaktirovatь-dannye-sotrudnika-/
+Назначить управленческого руководителя - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Naznachitь-upravlencheskogo-rukovoditelia/
+Уволить и заблокировать доступ - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Uvolitь-i-zablokirovatь-dostup-/
+Восстановить уволенного сотрудника - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Vosstanovitь-uvolennogo-sotrudnika--/
+Изменить канал подписания без перевыпуска сертификата УНЭП - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Izmenitь-kanal-polucheniia-odnorazovykh-kodov-/
+Изменить канал получения уведомлений о новых документах - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Izmenitь-kanal-polucheniia-uvedomlenii-o-novykh-dokumentakh-/
+Скачать XLS-реестр сотрудников - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Vygruzitь-XLS-reestr/
+Назначить заместителя - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Naznachitь-zamestitelia/
+Подключить сотруднику ПЭП Госуслуги для подписания документов на портале «Работа России» - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Podkliuchitь-sotrudniku-PEP-Gosuslugi-dlia-podpisaniia-dokumentov-na-portale-Rabota-Rossii/
+Отключить сотруднику уведомления в Telegram - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Otkliuchitь-sotrudniku-uvedomleniia-v-Telegram-/
+Юрлица - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Spravochniki-/IUrlitsa-/
+Отделы - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Spravochniki-/Otdely-/
+Должности - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Spravochniki-/Dolzhnosti-/
+Стандартные шаблоны заявлений - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Spravochniki-/Tipy-zaiavlenii-/Standartnye-shablony-zaiavlenii/
+Настроить тип заявления - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Spravochniki-/Tipy-zaiavlenii-/Nastroitь-tip-zaiavleniia-/
+Редактировать шаблон печатной формы заявления - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Spravochniki-/Tipy-zaiavlenii-/Redaktirovatь-shablon-pechatnoi-formy-zaiavleniia/
+Типы документов - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Spravochniki-/Tipy-dokumentov/
+@Обращения в службу заботы о клиентах (кадровик) - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/%40ObraSHCHeniia-v-sluzhbu-zaboty-o-klientakh-/
+1C ЗУП 3.1: Модуль интеграции - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/1C-ZUP-3.1%3A-Modulь-integratsii/
+1С ЗУП 3.1 и 1С Fresh: ВПФ - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/1S-ZUP-3.1%3A-VPF-/
+1С ЗУП 2.5: ВПФ - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/1S-ZUP-2.5%3A-VPF-/
+1C ЗУП 3.1: Расширение - https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/1C-ZUP-3.1%3A-Rasshirenie-/
+Назначить роль: Руководитель, Кадровик, Делопроизводитель - https://wiki.hr-link.ru/bin/view/Main/Administrator-/Sotrudniki-/Naznachitь-rolь%3A-Rukovoditelь%2C-Kadrovik%2C-Deloproizvoditelь-/
+Администратор, Назанчить роль: кадровик, Руководитель, Делопроизводитель; Наделить пользователя правами администратора и настройщика; управлять правами кадровиков и руководителей - https://wiki.hr-link.ru/bin/view/Main/Administrator-/Sotrudniki-/
+Администратор, спарвочники,  МЧД Создать МЧД вручную Создать МЧД из файла Копировать, редактировать и отправлять на подпись черновика МЧД Карточка МЧД: скачать, удалить, восстановить удалённую доверенность; Типы заявлений Создать новый тип заявления или редактировать существующий - https://wiki.hr-link.ru/bin/view/Main/Administrator-/Spravochniki-/
+
+
+STARTLINK - https://wiki.hr-link.ru/bin/view/Start%20Link/
+"""
+
+
+hr_link_docs = {
+    "Переключение в режим 'Сотрудник'": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Lichnyi-kabinet-/Perekliuchenie-v-rezhim-«Sotrudnik»-/",
+    "Загрузить документ": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Zagruzitь-dokument/",
+    "Настроить маршрут согласования документа": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Nastroitь-marshrut-soglasovaniia-dokumenta-/",
+    "Добавить в документ более одного сотрудника": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Dobavitь-v-dokument-bolee-odnogo-sotrudnika-/",
+    "Отправить документ на подпись": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Otpravitь-dokument-na-podpisь/",
+    "Удалить или аннулировать документ": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Udalitь-ili-annulirovatь-dokument-/",
+    "Скачать архив документооборота": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Skachatь-arkhiv-dokumentooborota-/",
+    "Скачать архивы КЭДО": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Massovoe-skachivanie-arkhivov-dokumentooborota/",
+    "Добавить комментарий к документу": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Dobavitь-kommentarii-k-dokumentu-/",
+    "Добавить заметки к документу": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Dobavitь-zametki-k-dokumentu/",
+    "Использовать фильтры в реестре документов": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Dokumenty-/Ispolьzovatь-filьtry-v-reestre-dokumentov/",
+    "Загрузить ЛНА": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/LNA-/Zagruzitь-LNA/",
+    "Утвердить ЛНА у руководителя": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/LNA-/Utverditь-LNA-u-rukovoditelia/",
+    "Отправить один ЛНА выбранным сотрудникам": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/LNA-/Otpravitь-odin-LNA-vybrannym-sotrudnikam/",
+    "Отправить несколько ЛНА выбранным сотрудникам": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/LNA-/Otpravitь-neskolьko-LNA-vybrannym-sotrudnikam/",
+    "Управлять параметрами ЛНА: автоотправка, префиксы, отображение после подписания": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/LNA-/Upravliatь-parametrami-LNA%3A-avtootpravka%2C-prefiksy%2C-otobrazhenie-posle-podpisaniia/",
+    "Настроить фильтры реестра": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Zaiavleniia/Nastroitь-filьtry-reestra/",
+    "Обработать или отклонить заявление": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Zaiavleniia/Obrabotatь-ili-otklonitь-zaiavlenie-/",
+    "Добавить связь между документом и заявлением": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Zaiavleniia/Dobavitь-sviazь-mezhdu-dokumentom-i-zaiavleniem-/",
+    "Добавить сотрудника на портал": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Dobavitь-sotrudnika-na-portal-/",
+    "Пригласить сотрудников на портал": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Priglasitь-sotrudnikov-na-portal-/",
+    "Проверка СМЭВ перед выпуском УНЭП": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Proverka-SMEV-pered-vypuskom-UNEP/",
+    "Инициировать выпуск УНЭП": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Vypustitь-UNEP-sotrudniku-/",
+    "Редактировать данные сотрудника": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Redaktirovatь-dannye-sotrudnika-/",
+    "Назначить управленческого руководителя": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Naznachitь-upravlencheskogo-rukovoditelia/",
+    "Уволить и заблокировать доступ": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Uvolitь-i-zablokirovatь-dostup-/",
+    "Восстановить уволенного сотрудника": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Vosstanovitь-uvolennogo-sotrudnika--/",
+    "Изменить канал подписания без перевыпуска сертификата УНЭП": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Izmenitь-kanal-polucheniia-odnorazovykh-kodov-/",
+    "Изменить канал получения уведомлений о новых документах": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Izmenitь-kanal-polucheniia-uvedomlenii-o-novykh-dokumentakh-/",
+    "Скачать XLS-реестр сотрудников": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Vygruzitь-XLS-reestr/",
+    "Назначить заместителя": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Naznachitь-zamestitelia/",
+    "Подключить сотруднику ПЭП Госуслуги для подписания документов на портале «Работа России»": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Podkliuchitь-sotrudniku-PEP-Gosuslugi-dlia-podpisaniia-dokumentov-na-portale-Rabota-Rossii/",
+    "Отключить сотруднику уведомления в Telegram": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Sotrudniki-/Otkliuchitь-sotrudniku-uvedomleniia-v-Telegram-/",
+    "Юрлица": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Spravochniki-/IUrlitsa-/",
+    "Отделы": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Spravochniki-/Otdely-/",
+    "Должности": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Spravochniki-/Dolzhnosti-/",
+    "Стандартные шаблоны заявлений": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Spravochniki-/Tipy-zaiavlenii-/Standartnye-shablony-zaiavlenii/",
+    "Настроить тип заявления": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Spravochniki-/Tipy-zaiavlenii-/Nastroitь-tip-zaiavleniia-/",
+    "Редактировать шаблон печатной формы заявления": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Spravochniki-/Tipy-zaiavlenii-/Redaktirovatь-shablon-pechatnoi-formy-zaiavleniia/",
+    "Типы документов": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/Spravochniki-/Tipy-dokumentov/",
+    "@Обращения в службу заботы о клиентах (кадровик)": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/%40ObraSHCHeniia-v-sluzhbu-zaboty-o-klientakh-/",
+    "1C ЗУП 3.1: Модуль интеграции": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/1C-ZUP-3.1%3A-Modulь-integratsii/",
+    "1С ЗУП 3.1 и 1С Fresh: ВПФ": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/1S-ZUP-3.1%3A-VPF-/",
+    "1С ЗУП 2.5: ВПФ": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/1S-ZUP-2.5%3A-VPF-/",
+    "1C ЗУП 3.1: Расширение": "https://wiki.hr-link.ru/bin/view/Main/Kadrovik-/1C-ZUP-3.1%3A-Rasshirenie-/",
+    "Назначить роль: Руководитель, Кадровик, Делопроизводитель": "https://wiki.hr-link.ru/bin/view/Main/Administrator-/Sotrudniki-/Naznachitь-rolь%3A-Rukovoditelь%2C-Kadrovik%2C-Deloproizvoditelь-/",
+    "Администратор, Назанчить роль: кадровик, Руководитель, Делопроизводитель; Наделить пользователя правами администратора и настройщика; управлять правами кадровиков и руководителей": "https://wiki.hr-link.ru/bin/view/Main/Administrator-/Sotrudniki-/",
+    "Администратор, справочники, МЧД Создать МЧД вручную Создать МЧД из файла Копировать, редактировать и отправлять на подпись черновика МЧД Карточка МЧД: скачать, удалить, восстановить удалённую доверенность; Типы заявлений Создать новый тип заявления или редактировать существующий": "https://wiki.hr-link.ru/bin/view/Main/Administrator-/Spravochniki-/",
+    "STARTLINK": "https://wiki.hr-link.ru/bin/view/Start%20Link/",
+    "Использовать фильтры в реестре кандидатов": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Kandidaty/Ispolьzovatь-filьtry-v-reestre-kandidatov/",
+    "Добавить кандидата": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Kandidaty/Dobavitь-kandidata-/",
+    "Стать ответственным за обработку кандидата": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Kandidaty/Statь-otvetstvennym-za-obrabotku-kandidata/",
+    "Пригласить кандидата в Start Link": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Kandidaty/Priglasitь-kandidata-v-Start-Link/",
+    "Запросить документы у кандидата": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Kandidaty/Zaprositь-dokumenty-u-kandidata/",
+    "Заполнить шаблон запроса документов за кандидата": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Kandidaty/Zapolnenie-shablona-zaprosa-dokumentov-za-kandidata/",
+    "Скачать отчёт о заполнение документов с анкетой кандидата": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Kandidaty/Skachatь-fail-otcheta-s-anketoi-kandidata/",
+    "Проверить кандидата на благонадежность": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Kandidaty/Proveritь-kandidata-na-blagonadezhnostь/",
+    "Передать кандидата в СБ для проверки на благонадёжность": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Kandidaty/Peredatь-kandidata-v-SB-dlia-proverki-na-blagonadezhnostь-/",
+    "Добавить город в карточку кандидата": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Kandidaty/Dobavitь-gorod-v-kartochku-kandidata/",
+    "Добавить теги к кандидату": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Kandidaty/Dobavitь-tegi-k-kandidatu/",
+    "Изменить статус кандидата": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Kandidaty/Statusy-kandidata/",
+    "Указать причину отказа при смене статуса кандидата на «Отказ»": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Kandidaty/Ukazatь-prichinu-otkaza-pri-smene-statusa-kandidata-na-Otkaz-/",
+    "Установка и обновлении обработки администратором 1С": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/1S%3A-Obrabotka-/Установка%20и%20обновлении%20обработки%20администратором%201С/",
+    "Авторизоваться с помощью API-токена (1С)": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/1S%3A-Obrabotka-/Avtorizovatьsia-s-pomoSHCHьiu-API-tokena-/",
+    "Импортировать кандидата в 1С": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/1S%3A-Obrabotka-/Importirovatь-kandidata-v-1S-/",
+    "Передать настройки соответствия полей всем пользователям (1С)": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/1S%3A-Obrabotka-/Peredatь-nastroiki-sootvetstviia-polei-vsem-polьzovateliam--/",
+    "Стандартизировать адрес прописки и передать в 1С": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/1S%3A-Obrabotka-/Poluchitь-adres-propiski-/",
+    "Передать банковские реквизиты физлица в 1С": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/1S%3A-Obrabotka-/Peredatь-bankovskie-rekvizity-fizlitsa-v-1S/",
+    "Шаблоны запроса": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Sozdatь-ili-redaktirovatь-shablon-zaprosa-dokumentov/",
+    "Шаблоны анкет": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Dobavitь-shablon-ankety/",
+    "Города (добавление в справочники, дальнейшие действия)": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kadrovik-/Goroda/",
+    "Удалить кандидата и его персональные данные": "https://wiki.hr-link.ru/bin/view/Start%20Link/Vladelets-sistemy-/Kandidaty/Udalitь-kandidata-i-ego-personalьnye-dannye/",
+    "Юридические лица (Добавить ЮЛ, добавить пользователя в ЮЛ, добавить кандидата в ЮЛ)": "https://wiki.hr-link.ru/bin/view/Start%20Link/Vladelets-sistemy-/Dobavitь-iurlitso-/",
+    "Пользователи(Добавить пользователя, пригласить пользователя)": "https://wiki.hr-link.ru/bin/view/Start%20Link/Vladelets-sistemy-/Dobavitь-i-redaktirovatь-polьzovatelia-/",
+    "Установить индикатор проверки физлица на благонадежность": "https://wiki.hr-link.ru/bin/view/Start%20Link/SB-/Kandidaty-/Ustanovitь-indikator-proverki-fizlitsa-na-blagonadezhnostь-/",
+    "Технические требования on-premises Start Link": "https://wiki.hr-link.ru/bin/view/Start%20Link/Tekhnicheskie-trebovaniia-on-premises-Start-Link/",
+    "Принять приглашение и заполнить шаблон запроса документов": "https://wiki.hr-link.ru/bin/view/Start%20Link/Kandidat-/Priniatь-priglashenie-i-zapolnitь-"
+}
